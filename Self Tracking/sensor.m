68b459d5d4c0eb97921a6cbd989e46425554b7e7
ShrimpCh1ps <sharonchenyoyohaha@gmail.com>
2023-12-19 03:21:59 +0800

    sensor_upload_gyro&mag

--- Self Tracking/sensor.m
+++ Self Tracking/sensor.m
@@ -1,11 +1,21 @@
-function [x_m, y_m] = gpsdata(x_t, y_t)
+function output = gpsdata(input)
     % 單位m
-    x_m = x_t + random('Normal', 0, 5/0.0987);
-    y_m = y_t + random('Normal', 0, 5/0.0987);
+    output(1) = input(1) + random('Normal', 0, 5);
+    output(2) = input(2) + random('Normal', 0, 5);
 end
 
-function [xdd_m, ydd_m] = acceldata(a)
+function output = acceldata(a)
     % 單位m/s^2 a=v^2/r
-    xdd_m = random('Normal', 0, 19.6/0.0987);
-    ydd_m = a + random('Normal', 0, 19.6/0.0987);
+    output(1) = random('Normal', 0, 19.6);
+    output(2) = a + random('Normal', 0, 19.6);
+end
+
+function output = gyrodata(input)
+    % 單位deg/s
+    output = 14/1000 + random('Normal', 0, 125);
+end
+
+function output = magdata(input)
+    % 單位uT 初始(?)
+    output = input + random('Normal', 0, 0.75);
 end
